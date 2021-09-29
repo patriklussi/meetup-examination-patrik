cc50d1e35365443381329d092dadd8f008a06cbb
patriklussi <46919194+patriklussi@users.noreply.github.com>
2021-09-29 12:58:39 +0200

    Fixed files and test file

--- meetup-app/src/App.vue
+++ meetup-app/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <MeetupList msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MeetupList from './components/MeetupList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MeetupList
   }
 }
 </script>
---
+++ meetup-app/tests/unit/meetupList.spec.js
@@ -0,0 +1,42 @@
+/*Du ska bygga en Vue-app för meetups och publicera den i en
+Docker-container på en molntjänst (Heroku). Användare ska kunna:
+● se kommande meetups,
+● anmäla sig till meetups,
+● recensera en meetup man har varit på
+*/
+
+
+
+
+
+import { shallowMount } from '@vue/test-utils'
+import MeetupList from '@/components/MeetupList.vue'
+
+let wrapper,data;
+beforeEach(()=>{
+  data = [ {name:"My Hero con",date:2022-05-20},{name:"Comic con",date:2021-11-21},{name:"Twitch-con",date:"2022-06-12",name:"Worlds 2021",date:"2021-10-05"} ]
+  wrapper = shallowMount("MeetupList",{
+    data:()=>{
+      meetupList:data,
+    }
+  })
+
+})
+
+describe('MeetupList', () => {
+  it('Should check if there are meetups in the list', () => {
+  
+    const 
+
+
+  });
+
+  it("Shold check that meetup info is legit",()=>{
+     
+  })
+
+
+  it("should be able to add meetup to localstorage",()=>{
+
+  })
+})
