2bf9ea1b485d860377aea03be6eb7246d40342bc
patriklussi <46919194+patriklussi@users.noreply.github.com>
2021-09-30 14:25:57 +0200

    Idk

--- meetup-app/tests/unit/meetupList.spec.js
+++ meetup-app/tests/unit/meetupList.spec.js
@@ -13,10 +13,11 @@ import VueRouter from 'vue-router'
 
 const localVue = createLocalVue()
 localVue.use(VueRouter);
+
 let wrapper,data;
 beforeEach(()=>{
   data = [ {name:"My Hero con",date:"2022-05-20"},{name:"Comic con",date:"2021-11-21"},{name:"Twitch-con",date:"2022-06-12",name:"Worlds 2021",date:"2021-10-05"} ]
-  wrapper = shallowMount("MeetList",{
+  wrapper = shallowMount(MeetList,{
     data:()=>{
       return {
         meetupList:data,
@@ -34,14 +35,15 @@ beforeEach(()=>{
   localStorage.setItem.mockClear();
 
   
-})
+});
 
 describe('MeetupList', () => {
   it('Should check if there are meetups in the list', () => {
   
     const meetupListCount = wrapper.findAll("section > article");
     const meetupList = wrapper.findAll("section > article").wrappers;
-    expect(MeetupList).toContain(data);
+    console.log(meetupList);
+    expect(meetupList).toContain(data);
     expect(meetupListCount.length).toBe(data.length);
 
 
@@ -50,7 +52,7 @@ describe('MeetupList', () => {
   it("should be able to add meetup to localstorage", async()=>{
     const KEY = "meetup";
     const VALUE = [{name:"My Hero con",date:"2022-05-20"}];
-      const addButton = wrapper.find(".addButton");
+      const addButton = wrapper.find("button");
       addButton.trigger("");
       expect(localStorage.setItem).toHaveBeenLastCalledWith(KEY,VALUE);
       expect(localStorage.__store__[KEY]).toBe(VALUE);
