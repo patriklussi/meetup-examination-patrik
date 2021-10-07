efd4d67683c0f26b6e944bab82b99cd7e8d0eafd
patriklussi <46919194+patriklussi@users.noreply.github.com>
2021-10-07 12:37:11 +0200

    khukhu

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -3,25 +3,31 @@ import 'jest-localstorage-mock';
 import App from "../../src/App.vue"
 
 
-let wrapper,data;
-beforeEach(()=>{
-  data = [ {name:"My Hero con",date:"2019-05-20",id:1,signedup:false,review:""},
-  {name:"Comic con",date:"2021-11-21",id:2,signedup:false,review:""},
-  {name:"Twitch-con",date:"2022-06-12",id:3,signedup:false,review:""},
-  {name:"Worlds 2021",date:"2021-10-05",id:4,signedup:false,review:""},
-   {name:"Batcon",date:"2021-11-11",id:5,signedup:false,review:""},
-   {name:"Superman-con",date:"2022-01-02",id:6,signedup:false,review:""},
-   {name:"Ice cream con",date:"2022-02-13",id:7,signedup:false,review:""},
-   {name:"Chips con", date:"2023-04-21",id:8,signedup:false,review:""} ],
-  wrapper = shallowMount(App,{
-    data: () => {
-      return {
-        meets: data,
-     }
-    
-}});
+
 
 describe("App.vue",()=>{
+    let wrapper,data;
+        beforeEach(()=>{
+            
+            wrapper = shallowMount(App,{
+                data =[ {name:"My Hero con",date:"2019-05-20",id:1,signedup:false,review:""},
+                {name:"Comic con",date:"2021-11-21",id:2,signedup:false,review:""},
+                {name:"Twitch-con",date:"2022-06-12",id:3,signedup:false,review:""},
+                {name:"Worlds 2021",date:"2021-10-05",id:4,signedup:false,review:""},
+                {name:"Batcon",date:"2021-11-11",id:5,signedup:false,review:""},
+                {name:"Superman-con",date:"2022-01-02",id:6,signedup:false,review:""},
+                {name:"Ice cream con",date:"2022-02-13",id:7,signedup:false,review:""},
+                {name:"Chips con", date:"2023-04-21",id:8,signedup:false,review:""} ],
+                data: () => {
+                    return {
+                      meets: data
+                }
+                }
+            })
+
+
+        })
+
 
     it("Should check that a localStorage commit happends", async()=>{
         const KEY = "meet";
--- tests/unit/meetupList.spec.js
+++ tests/unit/meetupList.spec.js
@@ -10,32 +10,23 @@ import MeetList from '@/components/MeetList.vue'
 
 
 
-
-
-
-let wrapper,data;
-beforeEach(()=>{
-  data = [ {name:"My Hero con",date:"2019-05-20",id:1,signedup:false,review:""},
-  {name:"Comic con",date:"2021-11-21",id:2,signedup:false,review:""},
-  {name:"Twitch-con",date:"2022-06-12",id:3,signedup:false,review:""},
-  {name:"Worlds 2021",date:"2021-10-05",id:4,signedup:false,review:""},
-   {name:"Batcon",date:"2021-11-11",id:5,signedup:false,review:""},
-   {name:"Superman-con",date:"2022-01-02",id:6,signedup:false,review:""},
-   {name:"Ice cream con",date:"2022-02-13",id:7,signedup:false,review:""},
-   {name:"Chips con", date:"2023-04-21",id:8,signedup:false,review:""} ],
-  wrapper = shallowMount(MeetList,{propsData : {
-    meetupList:data,
-   
-  }});
-
-
-  
-})
-  
-
-
-
 describe('MeetupList', () => {
+  let wrapper,data;
+  beforeEach(()=>{
+    data = [ {name:"My Hero con",date:"2019-05-20",id:1,signedup:false,review:""},
+    {name:"Comic con",date:"2021-11-21",id:2,signedup:false,review:""},
+    {name:"Twitch-con",date:"2022-06-12",id:3,signedup:false,review:""},
+    {name:"Worlds 2021",date:"2021-10-05",id:4,signedup:false,review:""},
+     {name:"Batcon",date:"2021-11-11",id:5,signedup:false,review:""},
+     {name:"Superman-con",date:"2022-01-02",id:6,signedup:false,review:""},
+     {name:"Ice cream con",date:"2022-02-13",id:7,signedup:false,review:""},
+     {name:"Chips con", date:"2023-04-21",id:8,signedup:false,review:""} ];
+     wrapper = shallowMount(MeetList,{propsData:{
+       meetupList:data
+     }})
+  })
+
+
   it('Should check if there are meetups in the list', () => {
   
     const meetupListCount = wrapper.findAll("section > article");
